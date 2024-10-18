f61154df7d7e5666f835b886fa83667bd2ac0b7a
cybermaxi <maximusagbe@gmail.com>
2024-10-18 17:52:41 +0100

    fixed issue of useSearchParams complaints in next.js needing to be wrapped in a suspence component

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
     Form,
@@ -12,14 +13,14 @@ import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { CiLock } from "react-icons/ci";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation"; // Still using useSearchParams
 import { supabase } from "@/lib/supabaseClient";
 import Link from "next/link";
 
+// Zod schema for form validation
 const formSchema = z.object({
     email: z.string().email("Please enter a valid email"),
     password: z.string().min(8, "Password must be at least 8 characters"),
@@ -27,11 +28,9 @@ const formSchema = z.object({
 
 export default function SignIn() {
     const router = useRouter();
-    const searchParams = useSearchParams(); // Hook to get search params (query)
-
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState(false); // Loading state
+    const [loading, setLoading] = useState(false);
 
     const handleTogglePassword = () => {
         setShowPassword(!showPassword);
@@ -51,7 +50,6 @@ export default function SignIn() {
         const { email, password } = values;
         setError(null);
 
-        // Supabase sign-in logic with email and password
         const { data, error } = await supabase.auth.signInWithPassword({
             email,
             password,
@@ -61,163 +59,160 @@ export default function SignIn() {
         if (error) {
             setError(error.message);
         } else {
-            // Store the session in localStorage if login is successful
             if (data.session) {
                 localStorage.setItem(
                     "supabaseSession",
                     JSON.stringify(data.session)
                 );
             }
-
-            // Get the redirectedFrom query parameter if available, or default to /dashboard
             const redirectedFrom = searchParams.get("redirectedFrom") || "/";
-
-            // Redirect to either the original page or the dashboard
             router.push(redirectedFrom);
         }
     };
 
     return (
-        <div className="min-h-screen relative flex items-center justify-center">
-            <div
-                className="absolute inset-0 bg-cover bg-center z-0"
-                style={{
-                    backgroundImage: `url('/images/loginBg.jpg')`,
-                }}
-            />
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="min-h-screen relative flex items-center justify-center">
+                <div
+                    className="absolute inset-0 bg-cover bg-center z-0"
+                    style={{
+                        backgroundImage: `url('/images/loginBg.jpg')`,
+                    }}
+                />
 
-            <Image
-                src="/icons/logo.svg"
-                alt="genyscope logo"
-                width={150}
-                height={63}
-                className="z-20 absolute top-[44px] left-[41px]"
-            />
+                <Image
+                    src="/icons/logo.svg"
+                    alt="genyscope logo"
+                    width={150}
+                    height={63}
+                    className="z-20 absolute top-[44px] left-[41px]"
+                />
 
-            <div
-                className="absolute inset-0 z-10"
-                style={{
-                    backgroundColor: "#1C7DB4",
-                    opacity: "0.70",
-                }}
-            />
+                <div
+                    className="absolute inset-0 z-10"
+                    style={{
+                        backgroundColor: "#1C7DB4",
+                        opacity: "0.70",
+                    }}
+                />
 
-            <section className="z-10 font-poppins text-white flex flex-col gap-6 px-2 md:px-0">
-                <header className="flex flex-col items-center justify-center">
-                    <h1 className="text-[35px] md:text-[45px] font-semibold text-center ">
-                        Welcome Back
-                    </h1>
-                    <p className="text-[16px] md:text-[20px] font-medium text-center">
-                        Kindly enter your sign-in credentials.
-                    </p>
-                </header>
+                <section className="z-10 font-poppins text-white flex flex-col gap-6 px-2 md:px-0">
+                    <header className="flex flex-col items-center justify-center">
+                        <h1 className="text-[35px] md:text-[45px] font-semibold text-center ">
+                            Welcome Back
+                        </h1>
+                        <p className="text-[16px] md:text-[20px] font-medium text-center">
+                            Kindly enter your sign-in credentials.
+                        </p>
+                    </header>
 
-                <div className="bg-white px-4 md:px-6 py-5 md:py-7 rounded-[15px] w-full sm:[600px] md:w-[700px]">
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="space-y-8 w-full"
-                        >
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <div className="form-item">
-                                        <FormLabel className="text-[14px] md:text-[17px] font-semibold tracking-[5%] text-[#1C1C1C]">
-                                            Email
-                                        </FormLabel>
-                                        <div className="relative w-full flex flex-col">
-                                            <FormControl>
-                                                <Input
-                                                    type="email"
-                                                    placeholder="Enter Email"
-                                                    className="input-class text-stone-800"
-                                                    {...field}
-                                                />
-                                            </FormControl>
-
-                                            <FormMessage className="form-message mt-2" />
-                                        </div>
-                                    </div>
-                                )}
-                            />
-
-                            <FormField
-                                control={form.control}
-                                name="password"
-                                render={({ field }) => (
-                                    <div className="form-item">
-                                        <FormLabel className="text-[14px] md:text-[17px] font-semibold tracking-[5%] text-[#1C1C1C]">
-                                            Password
-                                        </FormLabel>
-                                        <div className="relative w-full flex flex-col">
-                                            <span className="absolute left-2 top-1/3 transform -translate-y-1/2">
-                                                <CiLock className="text-[#707070] w-4 md:w-5 h-4 md:h-5" />
-                                            </span>
-
-                                            <span
-                                                className="absolute right-3 top-1/3 transform -translate-y-1/2 cursor-pointer"
-                                                onClick={handleTogglePassword}
-                                            >
-                                                {showPassword ? (
-                                                    <FaRegEyeSlash className="text-[#707070] w-4 md:w-5 h-4 md:h-5" />
-                                                ) : (
-                                                    <FaRegEye className="text-[#707070] w-4 md:w-5 h-4 md:h-5" />
-                                                )}
-                                            </span>
-
-                                            <FormControl>
-                                                <Input
-                                                    type={
-                                                        showPassword
-                                                            ? "text"
-                                                            : "password"
-                                                    }
-                                                    placeholder="Enter Password"
-                                                    className="pl-10 pr-10 input-class text-stone-800"
-                                                    {...field}
-                                                />
-                                            </FormControl>
-
-                                            <p className="text-[#1C7DB4] font-semibold text-[12px] md:text-[15px] self-end mt-1">
-                                                Forgot Password
-                                            </p>
-
-                                            <FormMessage className="text-xs md:text-sm text-red-500 mt-2" />
-                                        </div>
-                                    </div>
-                                )}
-                            />
-                            {error && (
-                                <p className="text-red-500 text-sm">{error}</p>
-                            )}
-
-                            <Button
-                                type="submit"
-                                className="block text-center w-full bg-[#F98E30] text-white font-bold text-[14px] md:text-[16px] py-3 rounded-lg"
-                                disabled={loading} // Disable button when loading
+                    <div className="bg-white px-4 md:px-6 py-5 md:py-7 rounded-[15px] w-full sm:[600px] md:w-[700px]">
+                        <Form {...form}>
+                            <form
+                                onSubmit={form.handleSubmit(onSubmit)}
+                                className="space-y-8 w-full"
                             >
-                                {loading ? "Loading..." : "Submit"}{" "}
-                                {/* Show loading text */}
-                            </Button>
-                            <p className="text-stone-800">
-                                Not a user?{" "}
-                                <Link
-                                    href="/sign-up"
-                                    className="text-blue-500 font-semibold"
-                                >
-                                    Sign Up
-                                </Link>{" "}
-                            </p>
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <div className="form-item">
+                                            <FormLabel className="text-[14px] md:text-[17px] font-semibold tracking-[5%] text-[#1C1C1C]">
+                                                Email
+                                            </FormLabel>
+                                            <div className="relative w-full flex flex-col">
+                                                <FormControl>
+                                                    <Input
+                                                        type="email"
+                                                        placeholder="Enter Email"
+                                                        className="input-class text-stone-800"
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage className="form-message mt-2" />
+                                            </div>
+                                        </div>
+                                    )}
+                                />
 
-                            <p className="text-stone-900 text-[12px] md:text-[15px] font-medium text-center">
-                                Copyright &copy; 2022 Gynescope Specialist
-                                Hospital
-                            </p>
-                        </form>
-                    </Form>
-                </div>
-            </section>
-        </div>
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <div className="form-item">
+                                            <FormLabel className="text-[14px] md:text-[17px] font-semibold tracking-[5%] text-[#1C1C1C]">
+                                                Password
+                                            </FormLabel>
+                                            <div className="relative w-full flex flex-col">
+                                                <span className="absolute left-2 top-1/3 transform -translate-y-1/2">
+                                                    <CiLock className="text-[#707070] w-4 md:w-5 h-4 md:h-5" />
+                                                </span>
+
+                                                <span
+                                                    className="absolute right-3 top-1/3 transform -translate-y-1/2 cursor-pointer"
+                                                    onClick={
+                                                        handleTogglePassword
+                                                    }
+                                                >
+                                                    {showPassword ? (
+                                                        <FaRegEyeSlash className="text-[#707070] w-4 md:w-5 h-4 md:h-5" />
+                                                    ) : (
+                                                        <FaRegEye className="text-[#707070] w-4 md:w-5 h-4 md:h-5" />
+                                                    )}
+                                                </span>
+
+                                                <FormControl>
+                                                    <Input
+                                                        type={
+                                                            showPassword
+                                                                ? "text"
+                                                                : "password"
+                                                        }
+                                                        placeholder="Enter Password"
+                                                        className="pl-10 pr-10 input-class text-stone-800"
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <p className="text-[#1C7DB4] font-semibold text-[12px] md:text-[15px] self-end mt-1">
+                                                    Forgot Password
+                                                </p>
+                                                <FormMessage className="text-xs md:text-sm text-red-500 mt-2" />
+                                            </div>
+                                        </div>
+                                    )}
+                                />
+
+                                {error && (
+                                    <p className="text-red-500 text-sm">
+                                        {error}
+                                    </p>
+                                )}
+
+                                <Button
+                                    type="submit"
+                                    className="block text-center w-full bg-[#F98E30] text-white font-bold text-[14px] md:text-[16px] py-3 rounded-lg"
+                                    disabled={loading}
+                                >
+                                    {loading ? "Loading..." : "Submit"}
+                                </Button>
+                                <p className="text-stone-800">
+                                    Not a user?{" "}
+                                    <Link
+                                        href="/sign-up"
+                                        className="text-blue-500 font-semibold"
+                                    >
+                                        Sign Up
+                                    </Link>
+                                </p>
+                                <p className="text-stone-900 text-[12px] md:text-[15px] font-medium text-center">
+                                    Copyright &copy; 2022 Gynescope Specialist
+                                    Hospital
+                                </p>
+                            </form>
+                        </Form>
+                    </div>
+                </section>
+            </div>
+        </Suspense>
     );
 }
